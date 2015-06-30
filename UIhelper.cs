7c97140a66cc127fe863cde3bb54064bdeee6b12
Thiago Sposito <sposito.thiago@gmail.com>
2015-06-30 12:40:14 +0100

    UIhelper class
    Creation of the class which provides UI tools

---
+++ UIhelper.cs
@@ -0,0 +1,15 @@
+class UIhelper{
+	public UIhelper()
+	{
+
+	}
+
+	public Vector2 calculatePivot(Sprite s){
+		float resultX, resultY;
+
+		resultX = s.pivot.x  / s.rect.width ;
+		resultY = s.pivot.y / s.rect.height ;
+
+		return new Vector2(resultX,resultY);
+	}
+}
